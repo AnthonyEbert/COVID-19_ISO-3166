97683e0c3972ff7acb30a8065df3150583657929
AnthonyEbert <anthonyebert@gmail.com>
2020-03-13 12:54:27 +0100

    Clean up.

--- verify_no_non-breaking-spaces.R
+++ verify_no_non-breaking-spaces.R
@@ -1,7 +1,7 @@
 
 library(dplyr)
 
-x <- read.csv("JohnsHopkins-to-A3_no-breaking-spaces.csv")$Country.Region %>%
+x <- read.csv("JohnsHopkins-to-A3.csv")$Country.Region %>%
   as.character()
 y <- gsub("\u00A0", " ", x, fixed = TRUE)
 
